c35705fcd9bf8a3205227735995daaf3670899ab
kajal <kajal@10.0.0.170>
2017-11-18 12:48:25 -0800

    Initial commit with DropdownSelect.java file

---
+++ src/workingWithWebElements/DropdownSelect.java
@@ -0,0 +1,68 @@
+package workingWithWebElements;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropdownSelect {
+
+	WebDriver driver;
+	String baseURL;
+
+	@Before
+	public void setUp() throws Exception {
+
+		baseURL = "https://letskodeit.teachable.com/p/practice";
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kajal\\Desktop\\Job\\Downloads\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get(baseURL);
+	}
+
+	@Test
+	public void dropdownOptions() throws InterruptedException {
+
+		WebElement element = driver.findElement(By.id("carselect"));
+
+		Select sel = new Select(element);
+		System.out.println("Select BMW");
+		sel.selectByValue("bmw");
+
+		Thread.sleep(2000);
+		System.out.println("Select Honda");
+		sel.selectByIndex(2);
+
+		Thread.sleep(2000);
+		System.out.println("Select Benz");
+		sel.selectByVisibleText("Benz");
+
+		List<WebElement> options = sel.getOptions();
+
+		int size = options.size();
+
+		for (int i = 0; i < size; i++) {
+			String optionName = options.get(i).getText();
+			System.out.println(optionName);
+		}
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
