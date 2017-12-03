3cb85b0606f217d340aaebb2a02fc92e29c7b554
kajalprakashpatil09@gmail.com <kajalprakashpatil09@gmail.com>
2017-12-03 13:20:55 -0800

    Added IDXpathDemo.java

--- src/findElements/IDXpathDemo.java
+++ src/findElements/IDXpathDemo.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/* 
+/* **
  * This class explains how to find web element by class name.
  */
 
